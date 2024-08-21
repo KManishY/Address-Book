d60ebdae3c034b0a828b2a2d5a104df235f143a6
kmanishy <my1000088@gmail.com>
2024-08-22 00:25:30 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import SignUp from './pages/Signup';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <div className="App">
+    <Login/>
+    <SignUp/>
+    </div>
+  );
+}
+
+export default App;
